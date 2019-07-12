7168a8bbf8f40a06f40b9971762e7d5746d84021
Matheus Cruz <matheuscruzp23@gmail.com>
2019-07-12 10:42:03 -0300

    Primeiro commit

---
+++ ControleChamado/Model/absProduto.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ControleChamado.Model
+{
+    public abstract class absProduto
+    {
+        //Atributos Proutos
+        private int _idProduto;
+        private string _produtoNome;
+
+        //Encapsulamento Produto
+        public string ProdutoNome
+        {
+            set { _produtoNome = value; }
+            get { return _produtoNome; }
+        }
+
+        public int IdProduto
+        {
+            get { return _idProduto; }
+        }
+    }
+}
